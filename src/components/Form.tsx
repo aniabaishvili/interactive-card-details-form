658169dfb33abb9963a0ec88357681ab96a841ab
ani <aniabaishvili1@gmail.com>
2024-06-03 14:34:07 +0400

    add form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Form(){
+    return(
+        <Containe>
+            <NamesInput></NamesInput>
+            <DateInput></DateInput>
+            <Button></Button>
+        </Containe>
+    )
+}
+
+export default Form;
